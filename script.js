cc6628343c9bc6e027b38ac9662783e1c8b912e7
Oduks-david <103524307+Oduks-david@users.noreply.github.com>
2022-12-21 14:15:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,170 @@
+ScrollReveal({
+    reset: true,
+    distance: '50px',
+    duration: '1000',
+    delay: 1000
+
+});
+
+
+ScrollReveal().reveal('.headline h1',
+    { delay: 20,
+      origin: 'bottom', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.me',
+    { delay: 100,
+      origin: 'bottom', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.words-layout p',
+    { delay: 120,
+      origin: 'bottom', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.second-background',
+    { delay: 800,
+      origin: 'bottom', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.shoe-headline h2',
+    { delay: 950,
+      origin: 'bottom', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.shoe-subheadline p',
+    { delay: 150,
+      origin: 'bottom', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.first-shoe img',
+    { delay: 1200,
+      origin: 'bottom', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.section-image img',
+    { delay: 100,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.section-image h1',
+    { delay: 500,
+      origin: 'bottom', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.section-image p',
+    { delay: 500,
+      origin: 'bottom', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.collaboration h1',
+    { delay: 500,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.collaboration p',
+    { delay: 600,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0.2
+});
+
+ScrollReveal().reveal('.collab-image',
+    { delay: 700,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.part-one h1',
+    { delay: 500,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.copyrights',
+    { delay: 600,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.info h3',
+    { delay: 700,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
+
+ScrollReveal().reveal('.info li',
+    { delay: 800,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.shop h3',
+    { delay: 700,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.shop li',
+    { delay: 800,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.about h3',
+    { delay: 700,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.about li',
+    { delay: 800,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
+ScrollReveal().reveal('.social a',
+    { delay: 700,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+ScrollReveal().reveal('.creators',
+    { delay: 800,
+      origin: 'left', 
+      interval: 300, 
+      opacity: 0
+});
+
